336972549a9ed8a1e7d726b19a3224120a78d85e
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2021-12-08 21:43:46 -0800

    test

---
+++ OnOffV2.lua
@@ -0,0 +1,31 @@
+local arg = ({...})
+
+local function setname()
+    local v = RandomNameNumber(10,20)
+    if (_G[v] == false) or (_G[v] == true) then
+        v = RandomNameNumber(10,20)
+        
+        setname()
+    else
+        _G[v] = false
+        return v
+    end
+end
+
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/NotRllyRn/Universal-loader/main/GuiLib.lua"))()
+
+do
+    local window = lib:CreateWindow("Strongest Punch Simulator")
+
+    window:CreateButton("Copy Discord Link",function()
+        setclipboard("https://discord.gg/zpFpWqBqCn")
+    end)
+    for _,v in pairs(arg) do
+        setName = setname()
+        window:CreateToggle(arg[1],function(val)
+            _G[setName] = val
+        end)
+    end
+end
+
+return globalName
